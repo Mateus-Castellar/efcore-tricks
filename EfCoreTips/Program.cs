694dee030442591ebfd097b6e47ddb3ea3e54917
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-10 03:52:40 -0300

    feat: Obter query sql gerada pelo EfCore

--- EfCoreTips/Program.cs
+++ EfCoreTips/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EfCoreTips.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreTips
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======= Dicas e Truques EfCore");
+            ObterSQlGerado();
+        }
+
+        static void ObterSQlGerado()
+        {
+            using var db = new ApplicationContext();
+            db.Database.EnsureCreated();
+
+            var query = db.Departamentos.Where(lbda => lbda.Id > 2);
+
+            var sql = query.ToQueryString();
+            Console.WriteLine(sql);
+        }
+    }
+}
