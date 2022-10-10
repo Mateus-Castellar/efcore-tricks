4c6176002ce83d7bc13505458a3d67d7ef9edea9
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-10 03:59:15 -0300

    feat: Redefinir estado do contexto

--- EfCoreTips/Program.cs
+++ EfCoreTips/Program.cs
@@ -11,6 +11,7 @@ namespace EfCoreTips
             Console.WriteLine("======= Dicas e Truques EfCore =======");
             //ObterSQlGerado();
             //DebugView();
+            //RedefinirEstadoContexto();
         }
 
         static void ObterSQlGerado()
@@ -33,6 +34,15 @@ namespace EfCoreTips
             var query = db.Departamentos.Where(lbda => lbda.Id > 2);
             //dentro do query teremos a propriedade "DebugView" onde estará contida nossa 
             //query e podemos obte-lá em modo de debug!
+        }
+
+        static void RedefinirEstadoContexto()
+        {
+            using var db = new ApplicationContext();
+
+            db.Departamentos.Add(new Departamento { Descricao = "teste debug view" });
+
+            db.ChangeTracker.Clear();
         }
     }
 }
